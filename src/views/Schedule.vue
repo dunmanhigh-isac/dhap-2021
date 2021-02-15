503b5a296646980bf1e59ca8f7b99e883fe32ac7
lizardon <chia.qien.don@dhs.sg>
2021-02-15 14:11:54 +0800

    schedule update

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -19,113 +19,101 @@
 
                         <!--put stuff here-->
 
-                        <div class="align-self-stretch grid grid-cols-1 mx-18 my-48 md:grid-cols-2 lg:grid-cols-3 gap-18 sm:p-8 my-18">
-                            <div class="item">
-                                <div id="timeline">
-                                    <div>
-                                        <section class="year">
-                                            <h3>2007</h3>
-                                            <section>
-                                                <ul>
-                                                    <li>Satoshi Nakamoto began working on the Bitcoin concept.</li>
-                                                </ul>
-                                            </section>
-                                        </section>
-                                        <section class="year">
-                                            <h3>2008</h3>
-                                            <section>
-                                                <h4>August</h4>
-                                                <ul>
-                                                    <li>Neal Kin, Vladimir Oksman, and Charles Bry file an application for an encryption patent application.</li>
-                                                    <li>Bitcoin.org was registered at <a href="https://anonymousspeech.com/">anonymousspeech.com</a>.</li>
-                                                </ul>
-                                            </section>
-                                            <section>
-                                                <h4>October</h4>
-                                                <ul>
-                                                    <li>Nakamoto describes the Bitcoin currency and solves the problem of double spending.</li>
-                                                </ul>
-                                            </section>
-                                            <section>
-                                                <h4>November</h4>
-                                                <ul>
-                                                    <li>The Bitcoin project is registered on <a href="https://sourceforge.net/">SourceForge.net</a>.</li>
-                                                </ul>
-                                            </section>
-                                        </section>
-                                    </div>
+                        <div class="align-self-stretch grid grid-cols-1 mx-18 my-48 md:grid-cols-2 lg:grid-cols-3 gap-18 sm:p-8 my-18 align-start">
+
+                                <div class="container-timeline">
+                                <div class="wrapper">
+                                    <div class="weight-900 text-24">02 Jun 2021</div>
+                                    <ul class="sessions">
+                                        <li>
+                                            <div class="time">0945 - 1000</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Technical Check</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1000 - 1115</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Opening Ceremony</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1130 - 1300</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Council Session 1</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1300 - 1430</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Lunch</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1430 - 1600</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Council Session 2</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1600 - 1630</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Tea Break</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1630 - 1800</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Council Session 3</div>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
-                            <div class="item">
-                                <div id="timeline">
-                                    <div>
-                                        <section class="year">
-                                            <h3>2007</h3>
-                                            <section>
-                                                <ul>
-                                                    <li>Satoshi Nakamoto began working on the Bitcoin concept.</li>
-                                                </ul>
-                                            </section>
-                                        </section>
-                                        <section class="year">
-                                            <h3>2008</h3>
-                                            <section>
-                                                <h4>August</h4>
-                                                <ul>
-                                                    <li>Neal Kin, Vladimir Oksman, and Charles Bry file an application for an encryption patent application.</li>
-                                                    <li>Bitcoin.org was registered at <a href="https://anonymousspeech.com/">anonymousspeech.com</a>.</li>
-                                                </ul>
-                                            </section>
-                                            <section>
-                                                <h4>October</h4>
-                                                <ul>
-                                                    <li>Nakamoto describes the Bitcoin currency and solves the problem of double spending.</li>
-                                                </ul>
-                                            </section>
-                                            <section>
-                                                <h4>November</h4>
-                                                <ul>
-                                                    <li>The Bitcoin project is registered on <a href="https://sourceforge.net/">SourceForge.net</a>.</li>
-                                                </ul>
-                                            </section>
-                                        </section>
-                                    </div>
+
+                            <div class="container-timeline">
+                                <div class="wrapper">
+                                    <div class="weight-900 text-24">03 Jun 2021</div>
+                                    <ul class="sessions">
+                                        <li>
+                                            <div class="time">0915 – 0930</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Technical Check</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">0930 - 1100</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Council Session 4</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1100 - 1300</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Lunch</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1300 - 1430</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Council Session 5</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1430 – 1500 </div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Tea Break</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1500 - 1630</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Council Session 6</div>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
-                            <div class="item">
-                                <div id="timeline">
-                                    <div>
-                                        <section class="year">
-                                            <h3>2007</h3>
-                                            <section>
-                                                <ul>
-                                                    <li>Satoshi Nakamoto began working on the Bitcoin concept.</li>
-                                                </ul>
-                                            </section>
-                                        </section>
-                                        <section class="year">
-                                            <h3>2008</h3>
-                                            <section>
-                                                <h4>August</h4>
-                                                <ul>
-                                                    <li>Neal Kin, Vladimir Oksman, and Charles Bry file an application for an encryption patent application.</li>
-                                                    <li>Bitcoin.org was registered at <a href="https://anonymousspeech.com/">anonymousspeech.com</a>.</li>
-                                                </ul>
-                                            </section>
-                                            <section>
-                                                <h4>October</h4>
-                                                <ul>
-                                                    <li>Nakamoto describes the Bitcoin currency and solves the problem of double spending.</li>
-                                                </ul>
-                                            </section>
-                                            <section>
-                                                <h4>November</h4>
-                                                <ul>
-                                                    <li>The Bitcoin project is registered on <a href="https://sourceforge.net/">SourceForge.net</a>.</li>
-                                                </ul>
-                                            </section>
-                                        </section>
-                                    </div>
+
+                            <div class="container-timeline">
+                                <div class="wrapper">
+                                    <div class="weight-900 text-24">04 Jun 2021</div>
+                                    <ul class="sessions">
+                                        <li>
+                                            <div class="time weight-900 text-24">0915 – 0930</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Technical Check</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">0930 - 1100</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Council Session 7</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1100 - 1230</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Lunch</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1230 - 1400</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Council Session 8</div>
+                                        </li>
+                                        <li>
+                                            <div class="time">1400 - 1600</div>
+                                            <div style="line-height: 1.5; margin-top: 0.4rem;">Closing Ceremony</div>
+                                        </li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
@@ -148,210 +136,87 @@
     border: 1px solid rgba( 255, 255, 255, 0.18);
 }
 
-a {
-    color: #50585f;
-    text-decoration: none;
+ul,
+li {
+    list-style: none;
+    padding: 0;
 }
 
-a:hover {
-    color: #383e44;
-}
-
-div.container {
+.container-timeline {
     display: flex;
-    flex: auto;
-    flex-direction: column;
-    max-height: 100%;
+    justify-content: center;
+    align-items: center;
 }
 
-div.header {
-    height: auto;
-    text-align: center;
-    background: slategrey;
-    color: ghostwhite;
-    padding: 2.3rem 1rem 2.3rem 1rem;
+.wrapper {
+    padding: 2rem;
+    border-radius: 15px;
+}
+
+h1 {
+    font-size: 1.1rem;
+    font-family: sans-serif;
+}
+
+.sessions {
+    margin-top: 2rem;
+    border-radius: 12px;
     position: relative;
 }
 
-div.header:after {
+li {
+    padding-bottom: 1.5rem;
+    border-left: 1px solid black;
+    position: relative;
+    padding-left: 20px;
+    margin-left: 10px;
+}
+
+li:last-child {
+    border: 0px;
+    padding-bottom: 0;
+}
+
+li:before {
     content: "";
-    position: absolute;
-    bottom: -5rem;
-    left: 0rem;
-    height: 5.1rem;
-    display: block;
-    width: 100%;
-    z-index: 300;
-    background: -moz-linear-gradient(top, white 20%, rgba(255, 255, 255, 0) 100%);
-    /* FF3.6-15 */
-    background: -webkit-linear-gradient(top, white 20%, rgba(255, 255, 255, 0) 100%);
-    /* Chrome10-25,Safari5.1-6 */
-    background: linear-gradient(to bottom, white 20%, rgba(255, 255, 255, 0) 100%);
-    /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr="#ffffff", endColorstr="#00ffffff", GradientType=0);
-    /* IE6-9 */
-}
-
-div.header h1 {
-    margin-top: 0.8rem;
-    margin-bottom: 0.5rem;
-    font-weight: 200;
-    font-size: 1.6em;
-    letter-spacing: 0.1rem;
-    text-transform: uppercase;
-}
-
-@media (min-width: 62em) {
-    div.header h1 {
-        font-size: 1.9em;
-        letter-spacing: 0.2rem;
-    }
-}
-
-div.header h2 {
-    font-size: 1.1em;
-    font-weight: 400;
-    color: #cfd7de;
-    max-width: 30rem;
-    margin: auto;
-}
-
-div.item {
-    display: flex;
-    flex: auto;
-    padding: 0rem 1rem 0rem 1rem;
-}
-
-#timeline {
-    position: relative;
-    display: table;
-    height: 100%;
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 5rem;
-}
-
-#timeline div:after {
-    content: "";
-    width: 2px;
-    position: absolute;
-    top: 0.5rem;
-    bottom: 0rem;
-    left: 60px;
-    z-index: 1;
-    background: #C5C5C5;
-}
-
-#timeline h3 {
-    position: -webkit-sticky;
-    position: sticky;
-    top: 5rem;
-    color: #888;
-    margin: 0;
-    font-size: 1em;
-    font-weight: 400;
-}
-
-@media (min-width: 62em) {
-    #timeline h3 {
-        font-size: 1.1em;
-    }
-}
-
-#timeline section.year {
-    position: relative;
-}
-
-#timeline section.year:first-child section {
-    margin-top: -1.3em;
-    padding-bottom: 0px;
-}
-
-#timeline section.year section {
-    position: relative;
-    padding-bottom: 1.25em;
-    margin-bottom: 2.2em;
-}
-
-#timeline section.year section h4 {
-    position: absolute;
-    bottom: 0;
-    font-size: 0.9em;
-    font-weight: 400;
-    line-height: 1.2em;
-    margin: 0;
-    padding: 0 0 0 89px;
-    color: #C5C5C5;
-}
-
-@media (min-width: 62em) {
-    #timeline section.year section h4 {
-        font-size: 1em;
-    }
-}
-
-#timeline section.year section ul {
-    list-style-type: none;
-    padding: 0 0 0 75px;
-    margin: -1.35rem 0 1em;
-    max-width: 32rem;
-    font-size: 1em;
-}
-
-@media (min-width: 62em) {
-    #timeline section.year section ul {
-        font-size: 1.1em;
-        padding: 0 0 0 81px;
-    }
-}
-
-#timeline section.year section ul:last-child {
-    margin-bottom: 0;
-}
-
-#timeline section.year section ul:first-of-type:after {
-    content: "";
-    width: 10px;
-    height: 10px;
-    background: #C5C5C5;
-    border: 2px solid #FFFFFF;
-    -webkit-border-radius: 50%;
-    -moz-border-radius: 50%;
-    -ms-border-radius: 50%;
+    width: 15px;
+    height: 15px;
+    background: black;
     border-radius: 50%;
     position: absolute;
-    left: 54px;
-    top: 3px;
-    z-index: 2;
+    left: -7px;
+    top: 0px;
 }
 
-#timeline section.year section ul li {
-    margin-left: 0.5rem;
+.time {
+    color: #2a2839;
+    font-family: "Poppins", sans-serif;
+    font-weight: 900;
 }
 
-#timeline section.year section ul li:before {
-    content: "·";
-    margin-left: -0.5rem;
-    padding-right: 0.3rem;
+@media screen and (min-width: 601px) {
+    .time {
+        font-size: 20px;
+    }
 }
 
-#timeline section.year section ul li:not(:first-child) {
-    margin-top: 0.5rem;
+@media screen and (max-width: 600px) {
+    .time {
+        margin-bottom: 0.3rem;
+    }
 }
 
-#timeline section.year section ul li span.price {
-    color: mediumturquoise;
-    font-weight: 500;
+p {
+    color: #4f4f4f;
+    font-family: sans-serif;
+    line-height: 1.5;
+    margin-top: 0.4rem;
 }
 
-#price {
-    display: inline;
-}
-
-svg {
-    border: 3px solid white;
-    border-radius: 50%;
-    box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+@media screen and (max-width: 600px) {
+    p {
+        font-size: 0.9rem;
+    }
 }
 </style>
 
