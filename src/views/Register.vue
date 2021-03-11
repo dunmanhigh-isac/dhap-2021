ffa34a4ce08ecb32b85b1d2c9077a00f7a441583
lizardon <chia.qien.don@dhs.sg>
2021-03-11 14:48:33 +0800

    dhs dels register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -17,8 +17,21 @@
             <div class="vstack my-48 mx-18">
 
                     <!--put stuff here-->
-                    <div class="align-self-stretch grid grid-cols-1 lg:mx-128 md:grid-cols-2 gap-18 sm:p-8" style="justify-content: center;
+                    <div class="align-self-stretch grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-18 sm:p-8" style="justify-content: center;
   align-items: center;">
+                        <div class="glass-card card vstack w-full h-auto card-bg rounded-3 shadow shadow-md card-shadow p-28 align-start space-28">
+                            <div class="weight-600 text-36">
+                                DHS Delegates
+                            </div>
+                            <div style="line-height: 1.4;">
+                                Registration for local delegates is now open and will close on 31st March 23:59 (GMT +08:00). Applicant's level of schooling should be between Year 3 and Year 6, at the time of sign up. DHAP will be free of charge for delegates from DHS. A merchandise package will be included.
+                            </div>
+                            <div class="spacer"></div>
+                            <a href="https://forms.gle/KPXTV2hHDqMUNDhG7" class="vstack px-36 py-12 shadow shadow-2xl rounded-8 bg-color button-hover button-click">
+                                Register
+                            </a>
+                        </div>
+
                         <div class="glass-card card vstack w-full h-auto card-bg rounded-3 shadow shadow-md card-shadow p-28 align-start space-28">
                             <div class="weight-600 text-36">
                                 Local Delegates
