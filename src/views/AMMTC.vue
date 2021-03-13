ba236c4329d385ebb5241b1317ab585bbd6f0d04
lizardon <chia.qien.don@dhs.sg>
2021-03-13 21:25:24 +0800

    ammtc changes

--- src/views/AMMTC.vue
+++ src/views/AMMTC.vue
@@ -27,57 +27,55 @@
                     </div>
     
                     <div class="vstack space-36 my-48 md:mx-128">
-                        <div class="weight-500 text-36 text-center">Response to Domestic Terrorism</div>
-                        <div class="text-20 text-justify" style="line-height: 1.2;">Given the rise in terrorist attacks around the world, ASEAN has placed emphasis on developing a regional counter-terrorism approach to strengthen regional security. The actions taken by ASEAN include the 2007 ASEAN Convention on Counter-Terrorism
-                            and the 2009 ASEAN Comprehensive Plan of Action on Counter-Terrorism, which aim to prevent and suppress terrorism within ASEAN by encouraging regional cooperation and research. Yet, there remain insufficiencies that undermine the
-                            effectiveness of such measures, as seen from the continued presence of terrorist groups like the Jemaah Islamiyah and the Daesh in Southeast Asian countries. Moreover, terrorists have also capitalised on technological tools to
-                            radicalise individuals. Therefore, delegates are to analyse and build on current ASEAN policies while coming up with innovative and effective solutions that can better address the threat of domestic terrorism.
-    
+                        <div class="weight-500 text-36 text-center">The Question of Curbing ASEAN’s Illicit Drug Trade</div>
+                        <div class="text-20 text-justify" style="line-height: 1.2;">Despite the dream of a Drug-Free ASEAN that was envisioned over 20 years ago, the problem of drug trafficking continues to grow in prominence - with more than 350,000 cases in 2019 alone. Given the wide-reaching repercussions of this
+                            illicit trade, it is crucial for ASEAN to take further action to curb the prevalence of drug trafficking. Yet, the increasingly sophisticated methods used to smuggle drugs, coupled with deeply rooted corruption and the lack of
+                            political enforcement, have greatly heightened the difficulty of tackling this issue. As such, delegates are tasked with formulating multifaceted solutions to curb drug trafficking within ASEAN.
                         </div>
                     </div>
     
                     <!-- <div class="vstack mt-128 mx-18 space-64 md:px-128">
-        
-                            <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
-                                <div class="w-full lg:w-512 bg-gray-400 h-320 mb-16"></div>
-                                <div class="hidden lg:unhidden spacer"></div>
-                                <div class="vstack align-start space-48 mt-16 w-full lg:pl-128">
-                                    <div class="w-128 h-20 bg-gray-300 rounded-full"></div>
-                                    <div class="vstack space-14 align-start">
-                                        <div class="w-full h-20 bg-gray-400 rounded-full"></div>
-                                        <div class="w-full h-20 bg-gray-400 rounded-full"></div>
+            
+                                <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
+                                    <div class="w-full lg:w-512 bg-gray-400 h-320 mb-16"></div>
+                                    <div class="hidden lg:unhidden spacer"></div>
+                                    <div class="vstack align-start space-48 mt-16 w-full lg:pl-128">
+                                        <div class="w-128 h-20 bg-gray-300 rounded-full"></div>
+                                        <div class="vstack space-14 align-start">
+                                            <div class="w-full h-20 bg-gray-400 rounded-full"></div>
+                                            <div class="w-full h-20 bg-gray-400 rounded-full"></div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-        
-                            <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
-        
-                                <div class="unhidden lg:hidden w-full lg:w-512 bg-gray-400 h-320 mb-16"></div>
-        
-                                <div class="vstack align-start lg:align-end space-48 lg:mt-16 w-full lg:pr-128">
-                                    <div class="w-128 h-20 bg-gray-300 rounded-full"></div>
-                                    <div class="vstack space-14 align-start">
-                                        <div class="w-full h-20 bg-gray-400 rounded-full"></div>
-                                        <div class="w-full h-20 bg-gray-400 rounded-full"></div>
+            
+                                <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
+            
+                                    <div class="unhidden lg:hidden w-full lg:w-512 bg-gray-400 h-320 mb-16"></div>
+            
+                                    <div class="vstack align-start lg:align-end space-48 lg:mt-16 w-full lg:pr-128">
+                                        <div class="w-128 h-20 bg-gray-300 rounded-full"></div>
+                                        <div class="vstack space-14 align-start">
+                                            <div class="w-full h-20 bg-gray-400 rounded-full"></div>
+                                            <div class="w-full h-20 bg-gray-400 rounded-full"></div>
+                                        </div>
+                                    </div>
+                                    <div class="hidden lg:unhidden spacer"></div>
+                                    <div class="hidden lg:unhidden w-full lg:w-512 bg-gray-400 h-320 mb-16"></div>
+                                </div>
+            
+                                <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
+                                    <div class="w-full lg:w-512 bg-gray-400 h-320 mb-16"></div>
+                                    <div class="hidden lg:unhidden spacer"></div>
+                                    <div class="vstack align-start space-48 mt-16 w-full lg:pl-128">
+                                        <div class="w-128 h-20 bg-gray-300 rounded-full"></div>
+                                        <div class="vstack space-14 align-start">
+                                            <div class="w-full h-20 bg-gray-400 rounded-full"></div>
+                                            <div class="w-full h-20 bg-gray-400 rounded-full"></div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="hidden lg:unhidden spacer"></div>
-                                <div class="hidden lg:unhidden w-full lg:w-512 bg-gray-400 h-320 mb-16"></div>
-                            </div>
-        
-                            <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
-                                <div class="w-full lg:w-512 bg-gray-400 h-320 mb-16"></div>
-                                <div class="hidden lg:unhidden spacer"></div>
-                                <div class="vstack align-start space-48 mt-16 w-full lg:pl-128">
-                                    <div class="w-128 h-20 bg-gray-300 rounded-full"></div>
-                                    <div class="vstack space-14 align-start">
-                                        <div class="w-full h-20 bg-gray-400 rounded-full"></div>
-                                        <div class="w-full h-20 bg-gray-400 rounded-full"></div>
-                                    </div>
-                                </div>
-                            </div>
-        
-                        </div> -->
+            
+                            </div> -->
     
                     <div class="vstack my-128 mx-18 space-64">
                         <!-- change to mt-128 only -->
