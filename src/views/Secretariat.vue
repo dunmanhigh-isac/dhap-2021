958f38d0db1285467f52c2798c4687ee2131c40e
lizardon <chia.qien.don@dhs.sg>
2021-05-15 23:00:23 +0800

    secretariat and changes to grammar 2

--- src/views/Secretariat.vue
+++ src/views/Secretariat.vue
@@ -20,7 +20,7 @@
                     <div class="vstack mt-128 mx-18 space-64 md:px-128">
     
                         <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
-                            <img style="object-fit: cover" src="../static/Secretariat/Valene.jpeg" class="rounded-6 w-full lg:w-320 bg-gray-400 h-full mb-16">
+                            <img style="object-fit: cover" src="../static/Secretariat/Valene.webp" class="rounded-6 w-full lg:w-320 bg-gray-400 h-full mb-16">
                             <div class="hidden lg:unhidden spacer"></div>
                             <div class="vstack align-start space-18 mt-16 w-full lg:pl-64">
                                 <div class="text-36 weight-500 m-0 p-0">Lim Jia Xuan Valene</div>
@@ -30,18 +30,18 @@
     
                         <!-- <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
     
-                            <img style="object-fit: cover" src="../static/Secretariat/Caitlin.jpeg" class="rounded-6 unhidden lg:hidden w-full lg:w-320 bg-gray-400 h-full mb-16">
+                            <img style="object-fit: cover" src="../static/Secretariat/Caitlin.webp" class="rounded-6 unhidden lg:hidden w-full lg:w-320 bg-gray-400 h-full mb-16">
     
                             <div class="vstack align-start lg:align-end space-18 lg:mt-16 w-full lg:pr-64">
                                 <div class="text-36 weight-500 m-0 p-0">Caitlin Hayley Susanto</div>
                                 <div class="text-18 text-left lg:text-right" style="line-height: 1.2;">As a Y6 student in the arts stream, Caitlin is currently struggling to write her essays within impossibly short times, and has accepted that her handwriting will never be legible again. A massive nerd, she likes to design powerpoint slides for fun and writes angsty poetry when distressed. She is also severely sleep deprived. Please send help.</div>
                             </div>
                             <div class="hidden lg:unhidden spacer"></div>
-                            <img style="object-fit: cover" src="../static/Secretariat/Caitlin.jpeg" class="rounded-6 hidden lg:unhidden w-full lg:w-320 bg-gray-400 h-full mb-16"/>
+                            <img style="object-fit: cover" src="../static/Secretariat/Caitlin.webp" class="rounded-6 hidden lg:unhidden w-full lg:w-320 bg-gray-400 h-full mb-16"/>
                         </div>
 
                         <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
-                            <img style="object-fit: cover" src="../static/Secretariat/Hsinyu.jpeg" class="rounded-6 w-full lg:w-320 bg-gray-400 h-full mb-16">
+                            <img style="object-fit: cover" src="../static/Secretariat/Hsinyu.webp" class="rounded-6 w-full lg:w-320 bg-gray-400 h-full mb-16">
                             <div class="hidden lg:unhidden spacer"></div>
                             <div class="vstack align-start space-18 mt-16 w-full lg:pl-64">
                                 <div class="text-36 weight-500 m-0 p-0">Cheng Hsin Yu</div>
@@ -51,18 +51,18 @@
     
                         <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
     
-                            <img style="object-fit: cover" src="../static/Secretariat/Lecia.jpeg" class="rounded-6 unhidden lg:hidden w-full lg:w-320 bg-gray-400 h-full mb-16">
+                            <img style="object-fit: cover" src="../static/Secretariat/Lecia.webp" class="rounded-6 unhidden lg:hidden w-full lg:w-320 bg-gray-400 h-full mb-16">
     
                             <div class="vstack align-start lg:align-end space-18 lg:mt-16 w-full lg:pr-64">
                                 <div class="text-36 weight-500 m-0 p-0">Lecia Cheok Yun Zhen</div>
                                 <div class="text-18 text-left lg:text-right" style="line-height: 1.2;">Now in her sixth year at Dunman High, Lecia is still learning the act of juggling commitments and keeping up with the seemingly never-ending, fast-paced lectures and tutorials. Passionate about both the humanities and sciences, she is a PCME student who tries her best to believe that there are exceptions to the "Do What You Love" rhetoric. A veteran of (barely) functioning with less than four hours of sleep, she is also experienced at battling enemies in PUBG and sleepiness during lectures. Thanks to Covid-19, her hopes of chairing a DHAP council are dashed. However, Lecia has developed a newfound passion to contribute to the organising of DHAP to the best of her abilities, and hopes that delegates will enjoy a fulfilling MUN experience at DHAP 2021.</div>
                             </div>
                             <div class="hidden lg:unhidden spacer"></div>
-                            <img style="object-fit: cover" src="../static/Secretariat/Lecia.jpeg" class="rounded-6 hidden lg:unhidden w-full lg:w-320 bg-gray-400 h-full mb-16"/>
+                            <img style="object-fit: cover" src="../static/Secretariat/Lecia.webp" class="rounded-6 hidden lg:unhidden w-full lg:w-320 bg-gray-400 h-full mb-16"/>
                         </div>
 
                         <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
-                            <img style="object-fit: cover" src="../static/Secretariat/Lixuan.jpeg" class="rounded-6 w-full lg:w-320 bg-gray-400 h-full mb-16">
+                            <img style="object-fit: cover" src="../static/Secretariat/Lixuan.webp" class="rounded-6 w-full lg:w-320 bg-gray-400 h-full mb-16">
                             <div class="hidden lg:unhidden spacer"></div>
                             <div class="vstack align-start space-18 mt-16 w-full lg:pl-64">
                                 <div class="text-36 weight-500 m-0 p-0">Sim Li Xuan</div>
@@ -72,18 +72,18 @@
     
                         <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
     
-                            <img style="object-fit: cover" src="../static/Secretariat/Joseph.jpeg" class="rounded-6 unhidden lg:hidden w-full lg:w-320 bg-gray-400 h-full mb-16">
+                            <img style="object-fit: cover" src="../static/Secretariat/Joseph.webp" class="rounded-6 unhidden lg:hidden w-full lg:w-320 bg-gray-400 h-full mb-16">
     
                             <div class="vstack align-start lg:align-end space-18 lg:mt-16 w-full lg:pr-64">
                                 <div class="text-36 weight-500 m-0 p-0">Gan Ru Shern, Joseph</div>
                                 <div class="text-18 text-left lg:text-right" style="line-height: 1.2;">Just like what the council's name states, Joseph has to confess that life now is nothing but a crisis. Joseph thus seeks simpler pleasures life has to offer, such as food, sleep and lots of sugar. On the bright side (thankfully), MUNs and great peer support is what keeps his sanity. Overall, he sincerely hopes that all delegates will reap meaningful discussions and forge friendships during DHAP 2021.</div>
                             </div>
                             <div class="hidden lg:unhidden spacer"></div>
-                            <img style="object-fit: cover" src="../static/Secretariat/Joseph.jpeg" class="rounded-6 hidden lg:unhidden w-full lg:w-320 bg-gray-400 h-full mb-16"/>
+                            <img style="object-fit: cover" src="../static/Secretariat/Joseph.webp" class="rounded-6 hidden lg:unhidden w-full lg:w-320 bg-gray-400 h-full mb-16"/>
                         </div>
 
                         <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
-                            <img style="object-fit: cover" src="../static/Secretariat/ShuHao.jpeg" class="rounded-6 w-full lg:w-320 bg-gray-400 h-full mb-16">
+                            <img style="object-fit: cover" src="../static/Secretariat/ShuHao.webp" class="rounded-6 w-full lg:w-320 bg-gray-400 h-full mb-16">
                             <div class="hidden lg:unhidden spacer"></div>
                             <div class="vstack align-start space-18 mt-16 w-full lg:pl-64">
                                 <div class="text-36 weight-500 m-0 p-0">Shi Shu Hao </div>
@@ -93,14 +93,14 @@
     
                         <div v-scroll-reveal class="vstack lg:hstack w-full mx-18 scroll-card">
     
-                            <img style="object-fit: cover" src="../static/Secretariat/SayTuan.jpeg" class="rounded-6 unhidden lg:hidden w-full lg:w-320 bg-gray-400 h-full mb-16">
+                            <img style="object-fit: cover" src="../static/Secretariat/SayTuan.webp" class="rounded-6 unhidden lg:hidden w-full lg:w-320 bg-gray-400 h-full mb-16">
     
                             <div class="vstack align-start lg:align-end space-18 lg:mt-16 w-full lg:pr-64">
                                 <div class="text-36 weight-500 m-0 p-0">Lim Say Tuan</div>
                                 <div class="text-18 text-left lg:text-right" style="line-height: 1.2;">Say Tuan is chronically shy which explains his love for hiding amongst trees. Please do not expect that he can tell his barber how to cut his hair - he’ll accept whatever haircut. He hopes that council time will be filled with fun discussion and an opportunity to learn!</div>
                             </div>
                             <div class="hidden lg:unhidden spacer"></div>
-                            <img style="object-fit: cover" src="../static/Secretariat/SayTuan.jpeg" class="rounded-6 hidden lg:unhidden w-full lg:w-320 bg-gray-400 h-full mb-16"/>
+                            <img style="object-fit: cover" src="../static/Secretariat/SayTuan.webp" class="rounded-6 hidden lg:unhidden w-full lg:w-320 bg-gray-400 h-full mb-16"/>
                         </div> -->
     
                     </div>
